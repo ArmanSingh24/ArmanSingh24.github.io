4cab9790587a8b850b2bb3be428d625f74d337bb
Arman Singh <109358065+ArmanSingh24@users.noreply.github.com>
2023-11-10 15:04:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+function switchTheme() {
+  var theme = document.getElementById('theme');
+  var icon = document.getElementById('themeIcon');
+  if (theme.getAttribute('href') == 'themes/theme1.css') {
+    theme.href = 'themes/theme2.css';
+  } else {
+    theme.href = 'themes/theme1.css';
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  let time = document.getElementById("timenddate");
+
+  setInterval(() => {
+    let d = new Date();
+    let options = { year: 'numeric', month: 'short', day: 'numeric' };
+    time.innerHTML =  " &#128218; • " + "Kota, IN • " + d.toLocaleDateString('en-US', options) + " • " + d.toLocaleTimeString();
+  }, 1000);
+});
+
+ function calculateAge() {
+   var birthDate = new Date('2008-03-24');
+   var now = new Date();
+   var age = (now - birthDate) / (365.25 * 24 * 60 * 60 * 1000);
+   document.getElementById('age').innerHTML = age.toFixed(8);
+ }
+ setInterval(calculateAge, 50);
+
+window.onload = function () {
+  var myDiv = document.getElementById("nav-items");
+  var icon = document.getElementById("icon");
+
+  icon.addEventListener("click", function () {
+    if (myDiv.style.display === "block") {
+      myDiv.style.display = "none";
+    } else {
+      myDiv.style.display = "block";
+    }
+  });
+
+  document.addEventListener("click", function (event) {
+    if (event.target !== icon && myDiv.style.display === "block") {
+      myDiv.style.display = "none";
+    }
+  });
+};
